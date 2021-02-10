0f104fdd30968ecd352291f471cdf275ea6dfd7c
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-02-09 22:13:59 -0300

    exercicios digitalinovation

---
+++ ex_DDDTelefone.rb
@@ -0,0 +1,21 @@
+codigo = gets.strip.to_i
+
+case codigo
+when 61 then puts "Brasilia"
+
+when 71 then puts "Salvador"
+
+when 11 then puts "São Paulo" 
+
+when 21 then puts "Rio de Janeiro"
+
+when 32 then puts "Juiz de Fora"
+
+when 19 then puts "Campinas"
+
+when 27 then puts "Vitoria" 
+
+when 31 then puts "Belo Horizonte"
+
+else puts " DDD não encontrado  "
+end
